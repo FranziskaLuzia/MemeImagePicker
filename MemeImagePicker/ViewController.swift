b0697a306244089ff5ba634be985b66d50aa988b
FranziskaLuzia <franziskakammerl@Franziskas-MBP.fios-router.home>
2018-04-22 19:06:48 -0400

    Fixes

--- MemeImagePicker/ViewController.swift
+++ MemeImagePicker/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MemeEditorViewController.swift
 //  MemeImagePicker
 //
 //  Created by Franziska Kammerl on 3/18/18.
@@ -9,33 +9,42 @@
 import UIKit
 
 struct Meme {
+    let image: UIImage
     let topText: String?
     let bottomText: String?
-    let originalImage: UIImage
     
     func memedImage(from view: UIView, with size: CGSize) -> UIImage {
-        
         UIGraphicsBeginImageContext(size)
         view.drawHierarchy(in: view.frame, afterScreenUpdates: true)
-        
         let image = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return image
     }
 }
 
-class ViewController: UIViewController {
+class MemeEditorViewController: UIViewController {
 
 // BUTTON OUTLETS
     
     @IBOutlet weak var imagePickerView: UIImageView!
-    @IBOutlet weak var PickButton: UIBarButtonItem!
-    @IBOutlet weak var CameraPicker: UIBarButtonItem!
-    @IBOutlet weak var TopText: UITextField!
-    @IBOutlet weak var BottomText: UITextField!
+    
+    @IBOutlet weak var pickButton: UIBarButtonItem!
+    
+    @IBOutlet weak var cameraPicker: UIBarButtonItem!
+    
+    @IBOutlet weak var topText: UITextField!
+    
+    @IBOutlet weak var bottomText: UITextField!
+    
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    
     @IBOutlet weak var toolBar: UIToolbar!
+    
     @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    
+    private var meme: Meme?
+
     
     let pickerController = UIImagePickerController()
     private var image: UIImage? {
@@ -45,13 +54,12 @@ class ViewController: UIViewController {
             shareButton.isEnabled = image != nil
         }
     }
-    var memedImage: UIImage?
     
     let memeTextAttributes:[String: Any] = [
-        NSAttributedStringKey.strokeColor.rawValue: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
-        NSAttributedStringKey.foregroundColor.rawValue: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
         NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedStringKey.strokeWidth.rawValue: 3.0
+        NSAttributedStringKey.strokeWidth.rawValue: -3.0
     ]
 
     override func viewDidLoad() {
@@ -72,24 +80,23 @@ class ViewController: UIViewController {
     }
    
     private func setupTextFields() {
-        TopText.text = "Top Text"
-        BottomText.text = "Bottom Text"
-        [TopText, BottomText].forEach { button in
-            button!.textAlignment = .center
-            button!.defaultTextAttributes = memeTextAttributes
+        [topText, bottomText].forEach { textField in
+            textField!.defaultTextAttributes = memeTextAttributes
+            textField!.textAlignment = .center
+            textField?.delegate = self
         }
     }
     
     private func setupImagePicker() {
         pickerController.delegate = self
         pickerController.sourceType = .photoLibrary
-        pickerController.allowsEditing = true
+        pickerController.allowsEditing = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         subscribeToKeyboardNotifications()
-        CameraPicker.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        cameraPicker.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -97,8 +104,19 @@ class ViewController: UIViewController {
         unsubscribeFromKeyboardNotifications()
     }
     
+    private var memedImage: UIImage {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.drawHierarchy(in: view.frame, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
     @objc func keyboardWillShow(_ notification: Notification) {
-        view.frame.origin.y = -getKeyboardHeight(notification)
+        let bottomEditing = bottomText.isEditing
+        if bottomEditing == true {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        } else { return }
     }
     
     @objc func keyboardWillHide(_ notification: Notification) {
@@ -121,22 +139,27 @@ class ViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
     
-    // BUTTON ACTIONS
-    
-    @IBAction func PickButtonPressed(_ sender: Any) {
-        present(pickerController, animated: true, completion: nil)
+    private func presentPicker(sourceType: UIImagePickerControllerSourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        imagePicker.sourceType = sourceType
+        present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func CameraPickerPressed(_ sender: Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        present(imagePicker, animated: true, completion: nil)
+    // BUTTON ACTIONS
+    
+    @IBAction func pickButtonPressed(_ sender: Any) {
+        presentPicker(sourceType: .photoLibrary)
+    }
+    
+    @IBAction func cameraPickerPressed(_ sender: Any) {
+        presentPicker(sourceType: .camera)
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
         save()
-        UIImageWriteToSavedPhotosAlbum(memedImage!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
+    
     
     @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error = error {
@@ -151,31 +174,38 @@ class ViewController: UIViewController {
     }
     
     @IBAction func shareButtonPressed(_ sender: Any) {
-        save()
+        changeChrome(isHidden: true)
         let activityView = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
-        self.present(activityView, animated: true, completion: nil)
-        
-        activityView.completionWithItemsHandler = {(activityType: UIActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
-            self.toolBar.isHidden = false
-            self.navigationController?.navigationBar.isHidden = false
-            if !completed {
+        activityView.completionWithItemsHandler = { (activityType: UIActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
+            self.changeChrome(isHidden: false)
+            if completed {
+                self.save()
+            } else {
                 self.dismiss(animated: true, completion: nil)
                 return
             }
         }
+    
+        self.present(activityView, animated: true, completion: nil)
     }
     
     private func save() {
-        toolBar.isHidden = true
-        navigationController?.navigationBar.isHidden = true
         guard let image = image else { return }
-        let meme = Meme(topText: TopText.text, bottomText: BottomText.text, originalImage: image)
-        memedImage = meme.memedImage(from: view, with: view.frame.size)
+        changeChrome(isHidden: true)
+        let meme = Meme(image: image, topText: topText.text, bottomText: bottomText.text)
+        let memedImage = meme.memedImage(from: view, with: view.frame.size)
+        UIImageWriteToSavedPhotosAlbum(memedImage, self, nil, nil)
+        changeChrome(isHidden: false)
+    }
+    
+    private func changeChrome(isHidden: Bool) {
+        toolBar.isHidden = isHidden
+        navigationController?.navigationBar.isHidden = isHidden
     }
 }
 
 // MARK: IMAGE PICKER
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+extension MemeEditorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as? UIImage
@@ -190,8 +220,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
 
 
 // MARK: TEXTFIELD DELEGATE
-extension ViewController: UITextFieldDelegate {
-    
+extension MemeEditorViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
